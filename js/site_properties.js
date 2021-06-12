f335792f38f68eb9f48028984bb14004f490ecd2
ok97 <omkhawshi0@gmail.com>
2021-06-12 15:25:23 +0530

    Added: Test the REST Services with JSON Server

---
+++ js/site_properties.js
@@ -0,0 +1,7 @@
+
+let site_properties={ // js file is defined to help reducing hard coded values in app and use dry principle
+    home_page: "../pages/EmployeePayrollHome.html",
+    emp_payroll_page:"../pages/EmployeePayrollApp.html",
+    use_local_storage:"false",
+    server_url: "  http://localhost:3000/EmployeePayrollDB/"
+};
